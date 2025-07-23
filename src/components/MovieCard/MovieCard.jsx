5de0f97451dcc90dd20775c456095754e6b2143f
Alessandro <Alessandrogabriel15160@gmail.com>
2025-07-23 18:59:08 -0300

    repair card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,8 +2,11 @@ import './MovieCard.scss';
 
 export const MovieCard = ({ movie }) => (
   <div className="card" data-cy="movie-card">
-    <img src={movie.imgUrl} alt={movie.title} data-cy="movie-poster" />
+    <a href={movie.imdbUrl} target="_blank" rel="noreferrer">
+      <img src={movie.imgUrl} alt={movie.title} data-cy="movie-poster" />
+    </a>
     <h2 data-cy="movie-title">{movie.title}</h2>
     <p data-cy="movie-year">{movie.year}</p>
+    <p>{movie.description}</p>
   </div>
 );
