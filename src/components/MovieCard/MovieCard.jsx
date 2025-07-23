3e749bcb3de3237ce4ad410760af20553e6c7d0b
Alessandro <Alessandrogabriel15160@gmail.com>
2025-07-23 18:38:22 -0300

    add solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,9 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ movie }) => (
+  <div className="card" data-cy="movie-card">
+    <img src={movie.imgUrl} alt={movie.title} data-cy="movie-poster" />
+    <h2 data-cy="movie-title">{movie.title}</h2>
+    <p data-cy="movie-year">{movie.description}</p>
+  </div>
+);
