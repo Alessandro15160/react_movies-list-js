8c07fd4b1f0f17602c0c041fd40d7294516c0db1
Alessandro <Alessandrogabriel15160@gmail.com>
2025-07-23 18:46:50 -0300

    add movie.year

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,6 +4,6 @@ export const MovieCard = ({ movie }) => (
   <div className="card" data-cy="movie-card">
     <img src={movie.imgUrl} alt={movie.title} data-cy="movie-poster" />
     <h2 data-cy="movie-title">{movie.title}</h2>
-    <p data-cy="movie-year">{movie.description}</p>
+    <p data-cy="movie-year">{movie.year}</p>
   </div>
 );
